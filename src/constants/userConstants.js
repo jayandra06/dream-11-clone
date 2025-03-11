ce74d13f447bd1ac245455f290b34c8e41a6f994
jayandra06 <68438965+jayandra06@users.noreply.github.com>
2025-03-11 22:31:47 +0530

    Update userConstants.js

--- src/constants/userConstants.js
+++ src/constants/userConstants.js
@@ -63,7 +63,7 @@ function geturl() {
   }
   // return "https://dreamelevenclonebackend-7l34.onrender.com";
   //return "https://dreamelevenclonebackend-1v8e.onrender.com";
-  return "https://dream11-clone-backend.vercel.app";
+  return "https://wonderwinbackend.leonxtgentech.com";
   // return "https://thepowerplay11-env.eba-ev2x8aa4.ap-south-1.elasticbeanstalk.com";
   // return "http://35.78.205.163";
   // return "http://dream-env.eba-6d6ds7up.us-east-1.elasticbeanstalk.com";
